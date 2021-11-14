2659e7814be72d48ae72e66c8f97ea821c4b9bc9
Javier Botella <jbm78@alu.ua.es>
2021-11-14 23:08:53 +0100

    A ver si va

--- studio/examples/event_parameter.cpp
+++ studio/examples/event_parameter.cpp
@@ -29,21 +29,63 @@ int FMOD_Main()
     FMOD::Studio::Bank* sfxBank = NULL;
     ERRCHECK( system->loadBankFile(Common_MediaPath("SFX.bank"), FMOD_STUDIO_LOAD_BANK_NORMAL, &sfxBank) );
 
-    FMOD::Studio::EventDescription* eventDescription = NULL;
-    ERRCHECK( system->getEvent("event:/Desert Leitmotif", &eventDescription) );
+    // -----------------    DESERT
 
-    // Find the parameter once and then set by handle
-    // Or we can just find by name every time but by handle is more efficient if we are setting lots of parameters
-    FMOD_STUDIO_PARAMETER_DESCRIPTION paramDesc;
-    ERRCHECK( eventDescription->getParameterDescriptionByName("progreso", &paramDesc) );
-    FMOD_STUDIO_PARAMETER_ID surfaceID = paramDesc.id;
+    FMOD::Studio::EventDescription* eventDescDesert = NULL;
+    ERRCHECK( system->getEvent("event:/Desert Leitmotif", &eventDescDesert) );
 
-    FMOD::Studio::EventInstance* eventInstance = NULL;
-    ERRCHECK( eventDescription->createInstance(&eventInstance) );
+    // Encuentra el parametro del evento según su nombre y crea una instancia del evento
+    FMOD_STUDIO_PARAMETER_DESCRIPTION paramDesertProgreso;
+    ERRCHECK( eventDescDesert->getParameterDescriptionByName("progreso", &paramDesertProgreso) );
+    FMOD_STUDIO_PARAMETER_ID desertID = paramDesertProgreso.id;
 
-    // Make the event audible to start with
-    float surfaceParameterValue = 0.0f;
-    ERRCHECK( eventInstance->setParameterByID(surfaceID, surfaceParameterValue) );
+    FMOD::Studio::EventInstance* eventInstDesert = NULL;
+    ERRCHECK( eventDescDesert->createInstance(&eventInstDesert) );
+
+    // El parametro progreso empieza en 0
+    float progresoParameterValue = 0.0f;
+    ERRCHECK( eventInstDesert->setParameterByID(desertID, progresoParameterValue) );
+
+    // -----------------    JUNGLE
+
+    FMOD::Studio::EventDescription* eventDescJungle = NULL;
+    ERRCHECK( system->getEvent("event:/Selva", &eventDescJungle) );
+
+    // Encuentra el parametro del evento según su nombre y crea una instancia del evento
+    FMOD_STUDIO_PARAMETER_DESCRIPTION paramJungleHora;
+    ERRCHECK( eventDescJungle->getParameterDescriptionByName("hora", &paramJungleHora) );
+    FMOD_STUDIO_PARAMETER_ID horaID = paramJungleHora.id;
+
+    FMOD::Studio::EventInstance* eventInstJungle = NULL;
+    ERRCHECK( eventDescJungle->createInstance(&eventInstJungle) );
+
+    // El parametro hora empieza en 0
+    float horaParameterValue = 0.0f;
+    ERRCHECK( eventInstJungle->setParameterByID(horaID, horaParameterValue) );
+
+    // -----------------    ELEVATOR
+
+    FMOD::Studio::EventDescription* eventDescElevator = NULL;
+    ERRCHECK( system->getEvent("event:/Ascensor", &eventDescElevator) );
+
+    // Encuentra el parametro del evento según su nombre y crea una instancia del evento
+    FMOD_STUDIO_PARAMETER_DESCRIPTION paramElevatorNivel;
+    ERRCHECK( eventDescElevator->getParameterDescriptionByName("nivel", &paramElevatorNivel) );
+    FMOD_STUDIO_PARAMETER_ID nivelID = paramElevatorNivel.id;
+
+    FMOD::Studio::EventInstance* eventInstElevator = NULL;
+    ERRCHECK( eventDescElevator->createInstance(&eventInstElevator) );
+
+    // El parametro nivel empieza en 0
+    float nivelParameterValue = 0.0f;
+    ERRCHECK( eventInstElevator->setParameterByID(nivelID, nivelParameterValue) );
+
+
+    // ----------------- VARIABLE
+    FMOD::Studio::EventInstance* eventInst = eventInstDesert;
+    FMOD_STUDIO_PARAMETER_DESCRIPTION paramEvent = paramDesertProgreso;
+    FMOD_STUDIO_PARAMETER_ID paramID = desertID; 
+    float paramValue = progresoParameterValue;
 
     do
     {
@@ -51,38 +93,67 @@ int FMOD_Main()
 
         if (Common_BtnPress(BTN_MORE))
         {
-            ERRCHECK( eventInstance->start() );
+            ERRCHECK( eventInst->start() );
         }
 
         if (Common_BtnPress(BTN_ACTION1))
         {
-            surfaceParameterValue = Common_Max(paramDesc.minimum, surfaceParameterValue - 1.0f);
-            ERRCHECK( eventInstance->setParameterByID(surfaceID, surfaceParameterValue) );
+            paramValue = Common_Max(paramEvent.minimum, paramValue - 1.0f);
+            ERRCHECK( eventInst->setParameterByID(paramID, paramValue) );
         }
 
         if (Common_BtnPress(BTN_ACTION2))
         {
-            surfaceParameterValue = Common_Min(surfaceParameterValue + 1.0f, paramDesc.maximum);
-            ERRCHECK( eventInstance->setParameterByID(surfaceID, surfaceParameterValue) );
+            paramValue = Common_Min(paramValue + 1.0f, paramEvent.maximum);
+            ERRCHECK( eventInst->setParameterByID(paramID, paramValue) );
+        }
+
+        if (Common_BtnPress(BTN_LEFT)) // DESERT
+        {
+            eventInst = eventInstDesert;
+            paramEvent = paramDesertProgreso;
+            paramID = desertID;
+            paramValue = progresoParameterValue;
+        }
+
+        if (Common_BtnPress(BTN_RIGHT)) // JUNGLE
+        {
+            eventInst = eventInstJungle;
+            paramEvent = paramJungleHora;
+            paramID = horaID;
+            paramValue = horaParameterValue;
+        }
+
+        if (Common_BtnPress(BTN_UP)) // ELEVATOR
+        {
+            eventInst = eventInstElevator;
+            paramEvent = paramElevatorNivel;
+            paramID = nivelID;
+            paramValue = nivelParameterValue;
         }
 
         ERRCHECK( system->update() );
 
         float userValue = 0.0f;
         float finalValue = 0.0f;
-        ERRCHECK( eventInstance->getParameterByID(surfaceID, &userValue, &finalValue) );
+        ERRCHECK( eventInst->getParameterByID(paramID, &userValue, &finalValue) );
 
         Common_Draw("==================================================");
         Common_Draw("Event Parameter Example.");
         Common_Draw("Copyright (c) Firelight Technologies 2012-2021.");
         Common_Draw("==================================================");
         Common_Draw("");
-        Common_Draw("Surface Parameter = (user: %1.1f, final: %1.1f)", userValue, finalValue);
+        Common_Draw("Parameter = (user: %1.1f, final: %1.1f)", userValue, finalValue);
         Common_Draw("");
-        Common_Draw("Surface Parameter:");
+        Common_Draw("Parameter:");
         Common_Draw("Press %s to play event", Common_BtnStr(BTN_MORE));
         Common_Draw("Press %s to decrease value", Common_BtnStr(BTN_ACTION1));
         Common_Draw("Press %s to increase value", Common_BtnStr(BTN_ACTION2));
+        Common_Draw("");
+        Common_Draw("Event:");
+        Common_Draw("Press %s to select Desert Event", Common_BtnStr(BTN_LEFT));
+        Common_Draw("Press %s to select Jungle Event", Common_BtnStr(BTN_RIGHT));
+        Common_Draw("Press %s to select Elevator Event", Common_BtnStr(BTN_UP));
         Common_Draw("");
         Common_Draw("Press %s to quit", Common_BtnStr(BTN_QUIT));
 
